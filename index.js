1bd10068817022d1a20fca81521c3fd7ac15d2d3
David Dias <daviddias.p@gmail.com>
2013-08-05 02:33:47 +0100

    added engines

---
+++ index.js
@@ -0,0 +1,18 @@
+var static = require('node-static');
+var blogMaker = require('blog-maker');
+
+blogMaker({ inputPath: 'data/posts'
+          , outputPath: 'public'
+          , templatePath: 'data/blog.html'
+          , rssTemplatePath: 'data/rss.xml'
+          , desiredBlogPath: 'blog'
+})
+
+var file = new(static.Server)('./public');
+
+require('http').createServer(function (request, response) {
+    request.on('end', function () {
+      file.serve(request, response)
+    })
+    request.resume()    
+}).listen(8080, function() { console.log('Listening on 8080')});
